f312e529fb08227a1bb149969c10304cc45fb4f1
Carwack <sybren.willemot@gmail.com>
2020-07-23 18:15:56 +0200

    📦 Add Bulma and Ml5 to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import ml5 from 'ml5';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-ml5-giphy';
+  classifier = {};
+  ngOnInit(): void {
+    this.classifier = ml5.imageClassifier('MobileNet', this.modelLoaded)
+  }
+
+  modelLoaded(): void {
+    console.log('model loaded')
+  }
 }
