3f4124836e21fa6cfff1b9851c7c6501365230c6
Carwack <sybren.willemot@gmail.com>
2020-08-04 17:04:18 +0200

    🚚 Add upload image and classify on it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, ViewChild } from '@angular/core'
 import ml5 from 'ml5'
 
 @Component({
@@ -8,8 +8,15 @@ import ml5 from 'ml5'
 })
 export class AppComponent implements OnInit {
   title = 'ng-ml5-giphy'
-  classifier = {}
+  classifier: { classify: (arg0: any, arg1: (err: any, results: any) => void) => void }
   isModelLoaded = false
+  result = {
+    label: '',
+    confidence: ''
+  }
+  @ViewChild('image') image: any
+  img: string
+  imgName = 'Upload an image to start'
 
   ngOnInit() {
     this.classifier = ml5.imageClassifier('MobileNet', this.modelLoaded.bind(this))
@@ -18,5 +25,29 @@ export class AppComponent implements OnInit {
   private modelLoaded() {
     console.log('model loaded')
     this.isModelLoaded = true
+  }
+
+  private classify() {
+    console.log(this.image)
+    this.classifier.classify(this.image.nativeElement, (err, results) => {
+      if (err) {
+        console.error(err)
+      } else {
+        console.log(results)
+        this.result.label = results[0].label
+        this.result.confidence = (results[0].confidence * 100).toFixed(2)
+      }
+    })
+  }
+
+  private uploadImage(event: { target: { files: any[] } }) {
+    const image = event.target.files[0]
+    const reader = new FileReader()
+    this.imgName = image.name
+    reader.readAsDataURL(image)
+    reader.onload = (e) => {
+      this.img = e.target.result.toString()
+      this.classify()
+    }
   }
 }
